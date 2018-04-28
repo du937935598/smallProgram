f55ea86e5d7eb00b89b31bdb1cfd2035f2aa3741
dubing <dubing@wanggejinfu.com>
2018-04-28 18:31:01 +0800

    20180428

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 //app.js
 App({
-
+  onLaunch: function (referrerInfo) {
+    console.log(referrerInfo);
+    console.log('app.js 初始化加载');
+  },
+  onShow: function (shareTicket) {
+    console.log(shareTicket)
+  },
+  onHide: function () {
+    console.log('隐藏')
+  },
+  name: '全局变量看看能否获取'
 })
--- pages/invest/invest.js
+++ pages/invest/invest.js
@@ -1,22 +1,18 @@
-// pages/home/home.js
-var app = getApp();
-
+// pages/invest/invest.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title: ''
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      title: app.name
-    })
+  
   },
 
   /**
