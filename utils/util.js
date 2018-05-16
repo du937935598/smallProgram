b758965684fda5f3029d64b95fb88b257d575c36
branton <937935598@qq.com>
2018-05-16 18:23:27 +0800

    27

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,11 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const del = val => {
+  return 100
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  del: del
 }
